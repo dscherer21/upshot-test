c1874b056c74111b33806e2129a715048079c92f
David Scherer-ODell <dscherer21@gmail.com>
2020-01-05 21:25:50 -0600

    adding layouts

--- client/src/components/body/body.js
+++ client/src/components/body/body.js
@@ -5,7 +5,60 @@ import './body.css';
 const Body = () => {
     return(
       <main className='container'>
-        
+        <div className='column'>
+            <a href='#' className='asidelink'>Footwear</a>
+            <a href='#' className='asidelink'>Water Fountains</a>
+            <a href='#' className='asidelink'>Garage Door Openers</a>
+            <a href='#' className='asidelink'>Smart Home Tech</a>
+            <a href='#' className='asidelink'>Beverages</a>
+            <a href='#' className='asidelink'>Education</a>
+        </div>
+        <div className='column'>
+            <div className='row'>
+                <div className='adbox'>
+                    <img src='#' alt='#'/>
+                    <h3>Title with Two Very Very Long Lines</h3>
+                    <p>And a very long description to boot. Why bother describing something with so many words?</p>
+                    <button>BUY NOW</button>
+                </div>
+            
+                <div className='adbox'>
+                    <img src='#' alt='#'/>
+                    <h3>One Short Title</h3>
+                    <p>And a very long description to boot. Why bother describing something with so many words?</p>
+                    <button>BUY NOW</button>
+                </div>
+
+                <div className='adbox'>
+                    <img src='#' alt='#'/>
+                    <h3>One Short Title</h3>
+                    <p>With a short description.</p>
+                    <button>BUY NOW</button>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='adbox'>
+                    <img src='#' alt='#'/>
+                    <h3>Title with Two Very Very Long Lines</h3>
+                    <p>With a short description.</p>
+                    <button>BUY NOW</button>
+                </div>
+            
+                <div className='adbox'>
+                    <img src='#' alt='#'/>
+                    <h3>Title with Two Very Very Long Lines</h3>
+                    <p>And a very long description to boot. Why bother describing something with so many words?</p>
+                    <button>BUY NOW</button>
+                </div>
+
+                <div className='adbox'>
+                    <img src='#' alt='#'/>
+                    <h3>Title with Two Very Very Long Lines</h3>
+                    <p>And a very long description to boot. Why bother describing something with so many words?</p>
+                    <button>BUY NOW</button>
+                </div>
+            </div>
+        </div>
       </main>
     )
 }
--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,10 +1,22 @@
 import React from "react";
 import './header.css';
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return(
       <header className='container'>
+        <div className='logoandlinks'>
+            <a href='#' className='logo'>
+                <img src='#' alt='#'/>
+            </a>
+            <div className='mainlinks'>
+                <a href='#' className='mainlinks1'>Home</a>
+                <a href='#' className='mainlinks1'>About</a>
+                <a href='#' className='mainlinks1'>Products</a>
+                <a href='#' className='mainlinks1'>Help</a>
+            </div>
+        </div>
+        <h1>THIS IS A HEADLINE.</h1>
         
       </header>
     )
