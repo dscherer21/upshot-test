45a7841ad8572787d6c8e51cc96884df28a0199d
David Scherer-ODell <dscherer21@gmail.com>
2020-01-05 18:54:35 -0600

    adding components folder

---
+++ client/src/components/body/body.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './body.css';
+//import { Link } from "react-router-dom";
+
+const Body = () => {
+    return(
+      <main className='container'>
+        
+      </main>
+    )
+}
+
+
+export default Body;
